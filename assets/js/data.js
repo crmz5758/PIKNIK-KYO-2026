e92c42b327365659c61de12dd935d1ff9727bf67
crmz17 <dimuscustom565@gmail.com>
2026-07-12 15:20:59 +0700

    Update data.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -555,13 +555,13 @@ const logKeluar = [
     {
         tanggal: "2026-07-12",
         jumlah: 4000000,
-        Metode: "Cash",
+        metode: "Cash",
         keperluan: "Pembayaran Transport Bus"
     },
     {
         tanggal: "2026-07-12",
         jumlah: 1250000,
-        Metode: "Cash",
+        metode: "Cash",
         keperluan: "Pembayaran Konsumsi Peserta"
     }
 ];
